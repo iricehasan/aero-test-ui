17d16be16ffd488282b0227855e453329c7faa77
iricehasan <hsn.irice@gmail.com>
2024-08-09 16:10:28 +0300

    update

--- contract/constants.ts
+++ contract/constants.ts
@@ -1,8 +1,8 @@
 export const INJ_DENOM = "factory/inj1v382yx63cyv4ezs0auh4h3c4u7rv766wsqx8et/INJ";
 export const USDT_DENOM = "factory/inj1v382yx63cyv4ezs0auh4h3c4u7rv766wsqx8et/USDT";
 
-export const cw20contractAddress = "inj1hhfyjmemw4tcxfgr06u0v0hjjjk0fy8dr0ppza"
+export const cw20contractAddress = "inj15ck89r95a05h7pt0jkjknwcnnkufdk09t9lphp"
+  
+export const oracleHelperAddress = "inj1ul4pfn3e9xetf465646x30p8dlr2ntwwp0zqu8"
 
-export const oracleHelperAddress = "inj1v3f7l45kmka6v4rsexy5re8rd7t2qp3gqcq5d6" 
-
-export const protocolAddress = "inj1qhmmgntkjpvsaz85kua05w86e6a272amhqjkyl"
+export const protocolAddress = "inj1elyap9ujkneuy0pntf0a4pkv7pdlzc9dk5uppy"
